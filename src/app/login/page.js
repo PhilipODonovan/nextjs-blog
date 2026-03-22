6627fc957f5bfc4784f6549c997cfb153d9f894f
PhilipODonovan <b00177493@mytudublin.ie>
2026-03-22 15:22:00 +0000

    login added

---
+++ src/app/login/page.js
@@ -0,0 +1,145 @@
+'use client';
+
+
+import Script from 'next/script'
+
+
+
+
+
+
+export default function Page() {
+
+
+  
+
+  /*
+  This function does the actual work
+  calling the fetch to get things from the database.
+  */ 
+  async function runDBCallAsync(url) {
+
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+ 
+    if(data.data== "valid"){
+      console.log("login is valid!")
+
+      
+    } else {
+
+      console.log("not valid  ")
+    }
+  }
+
+
+  /*
+
+  When the button is clicked, this is the event that is fired.
+  The first thing we need to do is prevent the default refresh of the page.
+  */
+	const handleSubmit = (event) => {
+		
+		console.log("handling submit");
+
+
+    event.preventDefault();
+  
+		const data = new FormData(event.currentTarget);
+
+
+    let email = data.get('email')
+		let pass = data.get('pass')
+
+    console.log("Sent email:" + email)
+    console.log("Sent pass:" + pass)
+
+
+    runDBCallAsync(`api/login?email=${email}&pass=${pass}`)
+
+
+
+
+  }; // end handler
+
+
+
+  return (
+
+    <>
+
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="Your Company"
+            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+            className="mx-auto h-10 w-auto"
+          />
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-500">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="pass" className="block text-sm/6 font-medium text-gray-500">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="pass"
+                  name="pass"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm/6 text-gray-400">
+            Not a member?{' '}
+            <a href="/register" className="font-semibold text-indigo-400 hover:text-indigo-300">
+              Register here
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+
+  );
+}
